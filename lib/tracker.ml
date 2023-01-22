38dd426d653ad6b12b8f1652ba484f22f75406a6
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-01-22 11:27:57 +0000

    Refactor to introduce read_line

--- lib/tracker.ml
+++ lib/tracker.ml
@@ -1,4 +1,4 @@
-let track_penpals list_file =
+let read_line list_file =
   let channel = open_in list_file in
 
   let _ = input_line channel in
@@ -7,6 +7,12 @@ let track_penpals list_file =
   let line = input_line channel in
 
   let _ = close_in channel in
+    line
 
-  let parts = String.split_on_char '|' line in
+let track_penpals list_file =
+  let parts =
+    list_file
+    |> read_line
+    |> String.split_on_char '|' in
+
     (String.concat "\n" parts) ^ "\n"
