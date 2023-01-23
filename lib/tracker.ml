29c2bf45e3d74fa4321dab57f55d21683cb7ae3a
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-01-23 19:18:01 +0000

    Rewrite read_line as a recursion

--- lib/tracker.ml
+++ lib/tracker.ml
@@ -1,13 +1,20 @@
 let read_line list_file =
+  let rec loop channel lines_so_far =
+    let line =
+      try Some (input_line channel) with
+      End_of_file -> None in
+
+    match line with
+    | Some (line) ->
+      loop channel (List.append lines_so_far [line])
+    | None -> lines_so_far in
+
   let channel = open_in list_file in
 
-  let _ = input_line channel in
-  let _ = input_line channel in
-
-  let line = input_line channel in
+  let lines = loop channel [] in
 
   let _ = close_in channel in
-    line
+    List.nth lines 2
 
 let parse_penpal line =
   String.split_on_char '|' line
