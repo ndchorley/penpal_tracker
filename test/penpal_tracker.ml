346e9cfd9ff83953c1f0e47d09e8bc5d48240762
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-01-24 20:11:12 +0000

    Add a layer of description to tests

--- test/penpal_tracker.ml
+++ test/penpal_tracker.ml
@@ -6,14 +6,16 @@ let contents_of file =
   open_in file |> input_all
 
 let tests =
-  "it produces a report of penpals" >::
-    (fun _ ->
-      let report = track_penpals "penpal_list" in
+  "tracking penpals" >::: [
+    "produces a report of penpals" >::
+      (fun _ ->
+        let report = track_penpals "penpal_list" in
 
-        assert_equal
-          (contents_of "expected")
-          report
-          ~printer:Fun.id
-    )
+          assert_equal
+            (contents_of "expected")
+            report
+            ~printer:Fun.id
+      )
+  ]
 
 let _ = run_test_tt_main tests
