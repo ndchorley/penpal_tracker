f5338a6b3c9230468ce01e3cc56cc9cd64aa8b10
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-01-26 19:39:39 +0000

    Move file handling code to its own module

---
+++ lib/file.ml
@@ -0,0 +1,22 @@
+let read_lines list_file =
+  let rec loop channel lines_so_far =
+    let line =
+      try Some (input_line channel) with
+      End_of_file -> None in
+
+    match line with
+    | Some (line) ->
+      loop channel (List.append lines_so_far [line])
+    | None -> lines_so_far in
+
+  let channel = open_in list_file in
+
+  let lines = loop channel [] in
+
+  let _ = close_in channel in
+      lines
+
+let drop_header lines =
+  lines
+  |> List.tl
+  |> List.tl
--- lib/tracker.ml
+++ lib/tracker.ml
@@ -1,29 +1,7 @@
+open File
 open Model
 open Parsing
 open Report
-
-let read_lines list_file =
-  let rec loop channel lines_so_far =
-    let line =
-      try Some (input_line channel) with
-      End_of_file -> None in
-
-    match line with
-    | Some (line) ->
-      loop channel (List.append lines_so_far [line])
-    | None -> lines_so_far in
-
-  let channel = open_in list_file in
-
-  let lines = loop channel [] in
-
-  let _ = close_in channel in
-      lines
-
-let drop_header lines =
-  lines
-  |> List.tl
-  |> List.tl
 
 let only_those_not_sent_a_letter penpals =
   List.filter
