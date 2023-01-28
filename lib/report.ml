4f5ddac8bae214c3ba06b37663e3d4ce91a4a402
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-01-28 12:13:28 +0000

    Refactor reporting code to abstract away the List.map

--- lib/report.ml
+++ lib/report.ml
@@ -1,9 +1,14 @@
 open Model
-let format_for_report penpal =
-  (String.concat "\n" [penpal.name; penpal.address]) ^ "\n"
+let format_for_report penpals =
+  List.map
+    (fun penpal ->
+      (String.concat
+        "\n" [penpal.name; penpal.address]) ^ "\n"
+    )
+    penpals
 
 let make_report penpals =
   penpals
-  |> List.map format_for_report
+  |> format_for_report
   |> String.concat "\n"
   |> String.cat "You have not written to:\n\n"
