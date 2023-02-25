a89a804d56e4f50d2195cc9fdbcf1f0f26a6773b
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-02-25 11:55:52 +0000

    Refactor pipeline to introduce run_smoke_tests

--- pipeline.sh
+++ pipeline.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
+. pipeline_lib.sh
+
 set -e
 
-./build.sh
-
-OS=$(uname)
-
-PYTHON="python3"
-
-if [ "$OS" = "Linux" ];
-then
-    PYTHON="python3.10"
-fi
-
-$PYTHON smoke_test.py
+./build.sh && run_smoke_tests
