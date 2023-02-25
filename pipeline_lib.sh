26df66b4d9bc4b09ff566061344764b993514fd8
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-02-25 12:09:32 +0000

    Refactor to introduce select_python_version

--- pipeline_lib.sh
+++ pipeline_lib.sh
@@ -1,12 +1,16 @@
-function run_smoke_tests() {
-    OS=$(uname)
+PYTHON="python3"
 
-    PYTHON="python3"
+function select_python_version() {
+    OS=$(uname)
 
     if [ "$OS" = "Linux" ];
     then
         PYTHON="python3.10"
     fi
+}
+
+function run_smoke_tests() {
+    select_python_version
 
     $PYTHON smoke_test.py
 }
