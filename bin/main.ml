9b330fd7f31635274126dbf15b7a79e5012d9cb0
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-02-26 12:29:52 +0000

    Introduce version module

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,12 @@
 open Penpal_tracker.Tracker
+open Version
 
 let contains array wanted =
   Array.exists (String.equal wanted) array
 
 let () =
   if (contains Sys.argv "--version") then
-    print_endline "penpal_tracker: some-commit-hash-here"
+    display_version_info
   else
     track_penpals "penpal_list"
     |> print_string
---
+++ bin/version.ml
@@ -0,0 +1,2 @@
+let display_version_info =
+  print_endline "penpal_tracker: some-commit-hash-here"
