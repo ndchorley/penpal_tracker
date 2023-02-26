fb31bf485918d455f7352f05b65140f7f4d4b9a3
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-02-26 13:16:08 +0000

    Fail smoke test if BUILD_COMMIT_HASH is empty

--- smoke_test.py
+++ smoke_test.py
@@ -5,13 +5,16 @@ import unittest
 
 class SmokeTest(unittest.TestCase):
     def test_it_displays_version_information(self):
+        expected_commit_hash = os.environ["BUILD_COMMIT_HASH"]
+
+        if not expected_commit_hash:
+            self.fail("BUILD_COMMIT_HASH was empty!")
+
         completed_process = subprocess.run(
             args=["_build/default/bin/main.exe", "--version"],
             capture_output=True,
             text=True
         )
-
-        expected_commit_hash = os.environ["BUILD_COMMIT_HASH"]
 
         self.assertEqual(
             completed_process.stdout,
