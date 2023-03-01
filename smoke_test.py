07251a8361bd221e4553c309f643c864d8f32ff1
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-03-01 19:40:19 +0000

    Remove check for empty BUILD_COMMIT_HASH in smoke tests

--- smoke_test.py
+++ smoke_test.py
@@ -7,9 +7,6 @@ class SmokeTest(unittest.TestCase):
     def test_it_displays_version_information(self):
         expected_commit_hash = os.environ["BUILD_COMMIT_HASH"]
 
-        if not expected_commit_hash:
-            self.fail("BUILD_COMMIT_HASH was empty!")
-
         completed_process = subprocess.run(
             args=["_build/default/bin/main.exe", "--version"],
             capture_output=True,
