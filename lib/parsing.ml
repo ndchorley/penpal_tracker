77f8c5d4b3ea5f86e4506033437b2677b05cfd5e
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-07-15 12:03:52 +0100

    Handle languages being in penpal list

--- lib/parsing.ml
+++ lib/parsing.ml
@@ -4,7 +4,7 @@ let parse_penpal line =
   let parts = (String.split_on_char '|' line) in
     {name = (List.nth parts 0);
      address = (List.nth parts 1);
-     letter_sent = (String.contains (List.nth parts 2) 'x')}
+     letter_sent = (String.contains (List.nth parts 3) 'x')}
 
 let parse_penpals lines_result =
   Result.map
