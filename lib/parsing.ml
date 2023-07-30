01602b0088b9259e5b8545714c22f8029e458703
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-07-30 10:17:49 +0100

    Split the languages into a list of strings while parsing the file

--- lib/parsing.ml
+++ lib/parsing.ml
@@ -1,10 +1,14 @@
 open Penpal
 
+let parse_languages languages_string =
+  languages_string
+  |> Str.split (Str.regexp ", ")
+
 let parse_penpal line =
   let parts = (String.split_on_char '|' line) in
     {name = (List.nth parts 0);
      address = (List.nth parts 1);
-     languages = [(List.nth parts 2)];
+     languages = parse_languages (List.nth parts 2);
      letter_sent = (String.contains (List.nth parts 3) 'x')}
 
 let parse_penpals lines_result =
